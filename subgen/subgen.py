b4579b7667a2c16c449b7dc6770e7bcc447ef682
ahyear <kevindelouya@gmail.com>
2023-03-03 15:04:13 +0100

    Update subgen.py

--- subgen/subgen.py
+++ subgen/subgen.py
@@ -120,10 +120,13 @@ def run_whisper(inputwav, finalsubname):
 
 def run_translate(finalsubname,targetlang):
     print("Starting translation")
+    with open("{}.srt".format(finalsubname), 'r') as extract:
+        source = extract.read(100)
+    print ("source : " +source)
     with open("{}.srt".format(finalsubname), 'r') as data:
         to_translate = data.read()
     translate = lt.translate("{}".format(to_translate),"en","{}".format(targetlang))
-    print ("translation : " + translate)
+    print ("translation : " + translate[:100])
     with open("{}.srt".format(finalsubname), 'w') as output_file:
         output_file.write(translate)
 
